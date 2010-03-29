41e20d8cda7d893693f9d0945bc3696e8ca7ea23
Todd Swindoll <tourach23@gmail.com>
2010-03-29 12:23:10 -0500

    fixed build fingerprint for market access

--- products/alldroid_sholes.mk
+++ products/alldroid_sholes.mk
@@ -6,7 +6,7 @@ PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := sholes-open
 PRODUCT_MODEL := Droid
 PRODUCT_MANUFACTURER := Motorola
-PRODUCT_BUILD_PROP_OVERRIDES := BUILD_ID=ERE25 BUILD_DISPLAY_ID="alldroid alpha 2.1 ERE25" PRODUCT_NAME=voles TARGET_DEVICE=sholes BUILD_FINGERPRINT=alldroid/alpha/sholes:2.0.1/ESD56 PRODUCT_BRAND=verizon
+PRODUCT_BUILD_PROP_OVERRIDES := BUILD_ID=ERE25 BUILD_DISPLAY_ID="alldroid alpha 2.1 ERE25" PRODUCT_NAME=voles TARGET_DEVICE=sholes BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.1-update1/ERE27/24178:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.1-update1 ERE27 24178 release-keys" PRODUCT_BRAND=verizon
 
 PRODUCT_PROPERTY_OVERRIDES += \
         ro.modversion=alldroid-alpha
